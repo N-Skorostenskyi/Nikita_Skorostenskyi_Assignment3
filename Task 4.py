bb604201fa84c23a6df6994e5f11ae66335d85dc
Nikita <nikita.skorostenskyi.25@kse.org.ua>
2026-07-09 14:35:57 +0300

    Task 4

---
+++ Task 4.py
@@ -0,0 +1,29 @@
+employees = [
+{"name": "Олена", "department": "Marketing", "salary":
+25000},
+{"name": "Ігор", "department": "IT", "salary": 55000},
+{"name": "Наталія", "department": "Marketing", "salary":
+28000},
+{"name": "Олег", "department": "HR", "salary": 22000},
+{"name": "Андрій", "department": "IT", "salary": 48000},
+{"name": "Марія", "department": "IT", "salary": 52000},
+]
+
+def get_department_stats(employee_list, target_dept):
+    emp_dept=[emp for emp in employee_list if emp["department"] == target_dept]
+
+    avg_salary=round(sum(emp["salary"] for emp in emp_dept)/len(emp_dept),2)
+
+    top_employee= max(emp_dept, key=lambda x: x["salary"])
+
+    count=len(emp_dept)
+    return {
+        "department": target_dept,
+        "average_salary": avg_salary,
+        "top_earner": top_employee["name"],
+        "count": count
+    }
+print(get_department_stats(employees,"IT"))
+
+
+
