240073e2a463d97073d20efbef9e25b69eac7f3e
Nikita <nikita.skorostenskyi.25@kse.org.ua>
2026-07-09 11:20:49 +0300

    Task 1 step 1

---
+++ Task 1.py
@@ -0,0 +1,11 @@
+book_profile = {
+"title": "Основи програмування",
+"author": "Іван Петренко",
+"year": 2023,
+"publisher_info": {
+"name": "Наукова думка",
+"city": "Київ"
+}
+}
+
+print(f"Назва: {book_profile["title"]}, автор: {book_profile["author"]}")
