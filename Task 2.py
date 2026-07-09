f5d837b8c8b915092fa0e53f56fc89b3a2dc4430
Nikita <nikita.skorostenskyi.25@kse.org.ua>
2026-07-09 13:12:17 +0300

    Task 2 step 1

---
+++ Task 2.py
@@ -0,0 +1,9 @@
+deposit_account = {
+"client_id": "C1025",
+"balance": 5000.0,
+"currency": "UAH",
+"interest_rate": 0.08, # 8% річних
+"is_active": True
+}
+
+print(f"Нараховані відсотки: {(deposit_account['balance']*deposit_account['interest_rate']):.2f}")
