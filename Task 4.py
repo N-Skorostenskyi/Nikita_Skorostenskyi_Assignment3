f3c16b872c3b6a5f93d9da55d31dd0565662e8c0
Nikita <nikita.skorostenskyi.25@kse.org.ua>
2026-07-10 15:17:25 +0300

    Task 4 creative task

--- Task 4.py
+++ Task 4.py
@@ -5,7 +5,7 @@ employees = [
 {"name": "Наталія", "department": "Marketing", "salary":
 28000},
 {"name": "Олег", "department": "HR", "salary": 22000},
-{"name": "Андрій", "department": "IT", "salary": 48000},
+{"name": "Андрій", "department": "IT", "salary": 55000},
 {"name": "Марія", "department": "IT", "salary": 52000},
 ]
 
@@ -14,13 +14,15 @@ def get_department_stats(employee_list, target_dept):
 
     avg_salary=round(sum(emp["salary"] for emp in emp_dept)/len(emp_dept),2)
 
-    top_employee= max(emp_dept, key=lambda x: x["salary"])
+    max_salary=max(emp["salary"] for emp in emp_dept)
+
+    top_employees= [emp["name"] for emp in emp_dept if emp["salary"]==max_salary]
 
     count=len(emp_dept)
     return {
         "department": target_dept,
         "average_salary": avg_salary,
-        "top_earner": top_employee["name"],
+        "top_earner": top_employees,
         "count": count
     }
 print(get_department_stats(employees,"IT"))
